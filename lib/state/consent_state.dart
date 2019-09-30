630d7e3e682bf5f530c110af204dcd80dedcbf35
Daniel Biedermann <biedermann@dipf.de>
2019-09-30 16:58:53 +0200

    add more content to consent

--- lib/state/consent_state.dart
+++ lib/state/consent_state.dart
@@ -1,3 +1,12 @@
-class ConsentState {
-  
+import 'package:flutter/widgets.dart';
+
+class ConsentState extends ChangeNotifier {
+  bool _consented = false;
+
+  get consented => _consented;
+
+  set consented(val) {
+    _consented = val;
+    notifyListeners();
+  }
 }
