4378a8838804616596b57dde05ad5916c08e2d56
Daniel Biedermann <biedermann@dipf.de>
2019-10-22 15:42:11 +0200

    add settings screen.

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:serene/widgets/serene_drawer.dart';
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: SereneDrawer(),
+      body: Container(),
+    );
+  }
+}
