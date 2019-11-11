cfb7c4325f6257293c68379dfc297e9aa46d26be
Daniel Biedermann <daniel.biedermann@cmore-automotive.com>
2019-11-11 09:22:46 +0100

    add goal shielding plea screen

---
+++ lib/screens/goal_shielding/goal_shielding_plea_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class GoalShieldingPleaScreen extends StatelessWidget {
+  const GoalShieldingPleaScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text(
+        "Versuche, den auf der folgenden Seite erscheinenden Satz zu verinnerlichen wenn du beim Lernen auf dein Hindernis stößt",
+        style: Theme.of(context).textTheme.display4,
+      ),
+    ));
+  }
+}
